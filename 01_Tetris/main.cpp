4491809106f8e07c22744c6ea5f0f724d8c8f777
SeonghuiChoe <heeday0410@gmail.com>
2020-01-15 19:32:05 +0900

    feat: update tiles in loop

--- 01_Tetris/main.cpp
+++ 01_Tetris/main.cpp
@@ -34,18 +34,21 @@ int main()
     window.clear(Color::White);
     window.draw(background);
 
-    /**
-     * sf::IntRect 클래스는 사각형을 표현하는 간단한 유틸리티 타입입니다.
-     * IntRect(좌표x, 좌표y, 가로, 세로)
-     * 이 클래스의 생성자는 왼쪽-위 모퉁이의 좌표와 사각형의 사이즈를 인자로 받습니다.
-     * setTextureRect는 sprite를 잘라서 사용
-     */
-    s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-    // 초기화 되어있는 x, y에 대입하기
-    s.setPosition(0, 0);
-    // 정사각형 바둑판으로 이동하기
-    s.move(28, 31);
-    window.draw(s);
+    for (int i = 0; i < 4; i++)
+    {
+      /**
+       * sf::IntRect 클래스는 사각형을 표현하는 간단한 유틸리티 타입입니다.
+       * IntRect(좌표x, 좌표y, 가로, 세로)
+       * 이 클래스의 생성자는 왼쪽-위 모퉁이의 좌표와 사각형의 사이즈를 인자로 받습니다.
+       * setTextureRect는 sprite를 잘라서 사용
+       */
+      s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+      // 초기화 되어있는 x, y에 대입하기
+      s.setPosition(a[i].x * 18, a[i].y * 18);
+      // 정사각형 바둑판으로 이동하기
+      s.move(28, 31);
+      window.draw(s);
+    }
     window.draw(frame);
     window.display();
   }
