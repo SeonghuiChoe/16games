e4f9d94dcd8d0a0d95156b1e31d43d01e5d83d24
SeonghuiChoe <heeday0410@gmail.com>
2020-01-16 22:00:49 +0900

    feat: make letter T

--- 01_Tetris/main.cpp
+++ 01_Tetris/main.cpp
@@ -2,14 +2,29 @@
 #include <time.h>
 using namespace sf;
 
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = {0};
+
 struct Point
 {
   int x, y;
 } a[4], b[4];
 
+int figures[7][4] =
+    {
+        1, 3, 5, 7, // I
+        2, 4, 5, 7, // Z
+        3, 5, 4, 6, // S
+        3, 5, 4, 7, // T
+        2, 3, 5, 7, // L
+        3, 5, 7, 6, // J
+        2, 3, 4, 5, // O
+};
+
 int main()
 {
-
   RenderWindow window(VideoMode(320, 480), "The Game!");
 
   Texture t1, t2, t3;
@@ -28,6 +43,13 @@ int main()
     {
       if (e.type == Event::Closed)
         window.close();
+    }
+
+    int n = 3;
+    for (int i = 0; i < 4; i++)
+    {
+      a[i].x = figures[n][i] % 2;
+      a[i].y = figures[n][i] / 2;
     }
 
     /////////draw//////////
