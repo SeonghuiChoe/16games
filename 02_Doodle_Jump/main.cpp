67f1c52c652c178749f763d6230b7c7c91053aa1
Seonghuichoe <heeday0410@gmail.com>
2020-01-27 19:00:39 +0900

    feat: show doodle

--- 02_Doodle_Jump/main.cpp
+++ 02_Doodle_Jump/main.cpp
@@ -1,16 +1,21 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
+struct point
+{
+  int x, y;
+};
+
 int main()
 {
   RenderWindow app(VideoMode(400, 533), "Doodle Game!");
-  app.setFramerateLimit(60);
 
-  Texture t1;
+  Texture t1, t2, t3;
   t1.loadFromFile("images/background.png");
+  t2.loadFromFile("images/platform.png");
+  t3.loadFromFile("images/doodle.png");
 
-  Sprite sBackground(t1);
-
+  Sprite sBackground(t1), sPlat(t2), sPers(t3);
 
   while (app.isOpen())
   {
@@ -21,8 +26,9 @@ int main()
         app.close();
     }
 
-   app.draw(sBackground);
-   app.display();
+    app.draw(sBackground);
+    app.draw(sPers);
+    app.display();
   }
   return 0;
 }
