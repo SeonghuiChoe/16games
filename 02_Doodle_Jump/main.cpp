5d7663435934f12acc121971711c34c2ed693ab0
SeonghuiChoe <heeday0410@gmail.com>
2020-02-03 19:46:41 +0900

    feat: move upstairs when step plats

--- 02_Doodle_Jump/main.cpp
+++ 02_Doodle_Jump/main.cpp
@@ -52,6 +52,20 @@ int main()
     if (y > 500)
       dy = -10;
 
+    // 발판을 밟았을 경우 화면을 위나 아래로 움직인다.
+    if (y < h)
+      for (int i = 0; i < 10; i++)
+      {
+        y = h;
+        plat[i].y = plat[i].y - dy;
+        if (plat[i].y > 533)
+        {
+          plat[i].y = 0;
+          plat[i].x = rand() % 400;
+        }
+      }
+
+    // 발판을 밟을 수 있도록한다.
     for (int i = 0; i < 10; i++)
       if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68) &&
           (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0))
