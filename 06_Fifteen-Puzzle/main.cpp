97b3399b70386041f36354fe0f7a4eb5d218370c
SeonghuiChoe <heeday0410@gmail.com>
2020-02-23 19:32:45 +0900

    feat: show numbers puzzle

--- 06_Fifteen-Puzzle/main.cpp
+++ 06_Fifteen-Puzzle/main.cpp
@@ -10,7 +10,7 @@ int main()
 
     Texture t;
     t.loadFromFile("images/15.png");
- 
+
     int w = 64;
     int grid[6][6] = {0};
     Sprite sprite[20];
@@ -32,37 +32,6 @@ int main()
         {
             if (e.type == Event::Closed)
                 app.close();
-
-               if (e.type == Event::MouseButtonPressed)
-                if (e.key.code == Mouse::Left)
-                  {
-                     Vector2i pos = Mouse::getPosition(app);
-                     int x = pos.x/w + 1;
-                     int y = pos.y/w + 1;
-
-                     int dx=0;
-                     int dy=0;
-
-                     if (grid[x+1][y]==16) {dx=1; dy=0;};
-                     if (grid[x][y+1]==16) {dx=0; dy=1;};
-                     if (grid[x][y-1]==16) {dx=0; dy=-1;};
-                     if (grid[x-1][y]==16) {dx=-1;dy=0;};
-
-                     int n = grid[x][y];
-                     grid[x][y] = 16;
-                     grid[x+dx][y+dy] = n;
-
-                     //animation
-                     sprite[16].move(-dx*w,-dy*w);
-                     float speed=3;
-
-                     for (int i=0;i<w;i+=speed)
-                     {sprite[n].move(speed*dx,speed*dy);
-                      app.draw(sprite[16]);
-                      app.draw(sprite[n]);
-                      app.display(); }
-                  }
-
         }
 
        app.clear(Color::White);
