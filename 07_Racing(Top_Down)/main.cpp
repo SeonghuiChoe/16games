cd5edec06744cebce10cb83479bae4b4c31955e9
SeonghuiChoe <heeday0410@gmail.com>
2020-02-28 17:42:06 +0900

    feat: move car

--- 07_Racing(Top_Down)/main.cpp
+++ 07_Racing(Top_Down)/main.cpp
@@ -12,10 +12,18 @@ int main()
   t2.loadFromFile("images/car.png");
 
   Sprite sBackground(t1), sCar(t2);
-  sCar.setPosition(300, 300);
+  sCar.setPosition(320, 340);
+  sCar.setOrigin(22, 22);
+
+  float x = 300, y = 300;
+  float speed = 0, angle = 0;
+  float maxSpeed = 12.0;
+  float acc = 0.2, dec = 0.3;
+  float turnSpeed = 0.08;
 
   while (app.isOpen())
   {
+
     Event e;
     while (app.pollEvent(e))
     {
@@ -23,8 +31,56 @@ int main()
         app.close();
     }
 
+    bool Up = 0, Right = 0, Down = 0, Left = 0;
+    if (e.key.code == Keyboard::Up)
+      Up = 1;
+    if (e.key.code == Keyboard::Right)
+      Right = 1;
+    if (e.key.code == Keyboard::Down)
+      Down = 1;
+    if (e.key.code == Keyboard::Left)
+      Left = 1;
+
+    //car movement
+    if (Up && speed < maxSpeed)
+    {
+      if (speed < 0)
+        speed += dec;
+      else
+        speed += acc;
+    }
+
+    if (Down && speed > -maxSpeed)
+    {
+      if (speed > 0)
+        speed -= dec;
+      else
+        speed -= acc;
+    }
+
+    if (!Up && !Down)
+    {
+      if (speed - dec > 0)
+        speed -= dec;
+      else if (speed + dec < 0)
+        speed += dec;
+      else
+        speed = 0;
+    }
+
+    if (Right && speed != 0)
+      angle += turnSpeed * speed / maxSpeed;
+    if (Left && speed != 0)
+      angle -= turnSpeed * speed / maxSpeed;
+
+    x += sin(angle) * speed;
+    y -= cos(angle) * speed;
+
     app.clear(Color::White);
     app.draw(sBackground);
+
+    sCar.setPosition(x, y);
+    sCar.setRotation(angle * 180 / 3.141592);
     sCar.setColor(Color::Red);
     app.draw(sCar);
 
