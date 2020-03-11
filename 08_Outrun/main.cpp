3c312da769b2aee49db1135f70e49ee64af4a7e6
SeonghuiChoe <heeday0410@gmail.com>
2020-03-11 18:36:17 +0900

    feat: move left and right

--- 08_Outrun/main.cpp
+++ 08_Outrun/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <math.h>
 using namespace sf;
 
@@ -7,6 +8,17 @@ int height = 768;
 int roadW = 2000;
 int segL = 200;    //segment length
 float camD = 0.84; //camera depth
+
+void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2)
+{
+    ConvexShape shape(4);
+    shape.setFillColor(c);
+    shape.setPoint(0, Vector2f(x1 - w1, y1));
+    shape.setPoint(1, Vector2f(x2 - w2, y2));
+    shape.setPoint(2, Vector2f(x2 + w2, y2));
+    shape.setPoint(3, Vector2f(x1 + w1, y1));
+    w.draw(shape);
+}
 
 struct Line
 {
@@ -29,17 +41,6 @@ struct Line
     }
 };
 
-void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2)
-{
-    ConvexShape shape(4);
-    shape.setFillColor(c);
-    shape.setPoint(0, Vector2f(x1 - w1, y1));
-    shape.setPoint(1, Vector2f(x2 - w2, y2));
-    shape.setPoint(2, Vector2f(x2 + w2, y2));
-    shape.setPoint(3, Vector2f(x1 + w1, y1));
-    w.draw(shape);
-}
-
 int main()
 {
     RenderWindow app(VideoMode(width, height), "Outrun Racing!");
@@ -55,7 +56,9 @@ int main()
     }
 
     int N = lines.size();
+    float playerX = 0;
     int pos = 0;
+    int H = 1500;
 
     while (app.isOpen())
     {
@@ -68,18 +71,33 @@ int main()
                 pos = +200;
             else if (e.key.code == Keyboard::Down)
                 pos = -200;
+            else if (e.key.code == Keyboard::Right)
+                playerX += 0.1;
+            else if (e.key.code == Keyboard::Left)
+                playerX -= 0.1;
         }
 
-        app.clear();
+        int speed = 0;
+
+        pos += speed;
+        while (pos >= N * segL)
+            pos -= N * segL;
+        while (pos < 0)
+            pos += N * segL;
+
         int startPos = pos / segL;
+        int camH = lines[startPos].y + H;
 
         int maxy = height;
+        float x = 0, dx = 0;
 
         ///////draw road////////
         for (int n = startPos; n < startPos + 300; n++)
         {
             Line &l = lines[n % N];
-            l.project(0, 1500, pos);
+            l.project(playerX * roadW - x, camH, startPos * segL - (n >= N ? N * segL : 0));
+            x += dx;
+            dx += l.curve;
 
             l.clip = maxy;
             if (l.Y >= maxy)
@@ -96,6 +114,7 @@ int main()
             drawQuad(app, rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2);
             drawQuad(app, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
         }
+
         app.display();
     }
 
