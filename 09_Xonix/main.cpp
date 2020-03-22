bd422d4d7413dc47e2797b0bd4bb07aea83a16fc
SeonghuiChoe <heeday0410@gmail.com>
2020-03-22 13:26:00 +0900

    feat: make green tile when red tile move

--- 09_Xonix/main.cpp
+++ 09_Xonix/main.cpp
@@ -24,6 +24,11 @@ int main()
   float timer = 0, delay = 0.07;
   Clock clock;
 
+  for (int i = 0; i < M; i++)
+    for (int j = 0; j < N; j++)
+      if (i == 0 || j == 0 || i == M - 1 || j == N - 1)
+        grid[i][j] = 1;
+
   while (window.isOpen())
   {
     float time = clock.getElapsedTime().asSeconds();
@@ -37,20 +42,61 @@ int main()
         window.close();
     }
 
-    if (e.key.code == Keyboard::Left) { dx = -1; dy = 0; }
-    if (e.key.code == Keyboard::Right) { dx = 1; dy = 0; }
-    if (e.key.code == Keyboard::Up) { dx = 0; dy = -1; }
-    if (e.key.code == Keyboard::Down) { dx = 0; dy = 1; }
+    if (e.key.code == Keyboard::Left)
+    {
+      dx = -1;
+      dy = 0;
+    }
+    if (e.key.code == Keyboard::Right)
+    {
+      dx = 1;
+      dy = 0;
+    }
+    if (e.key.code == Keyboard::Up)
+    {
+      dx = 0;
+      dy = -1;
+    }
+    if (e.key.code == Keyboard::Down)
+    {
+      dx = 0;
+      dy = 1;
+    }
 
     if (timer > delay)
     {
       x += dx;
       y += dy;
+
+      if (x < 0)
+        x = 0;
+      if (x > N - 1)
+        x = N - 1;
+      if (y < 0)
+        y = 0;
+      if (y > M - 1)
+        y = M - 1;
+
+      if (grid[y][x] == 0)
+        grid[y][x] = 2;
       timer = 0;
     }
 
     /////////draw//////////
     window.clear();
+
+    for (int i = 0; i < M; i++)
+      for (int j = 0; j < N; j++)
+      {
+        if (grid[i][j] == 0)
+          continue;
+        if (grid[i][j] == 1)
+          sTile.setTextureRect(IntRect(0, 0, ts, ts));
+        if (grid[i][j] == 2)
+          sTile.setTextureRect(IntRect(54, 0, ts, ts));
+        sTile.setPosition(j * ts, i * ts);
+        window.draw(sTile);
+      }
 
     sTile.setTextureRect(IntRect(36, 0, ts, ts));
     sTile.setPosition(x * ts, y * ts);
