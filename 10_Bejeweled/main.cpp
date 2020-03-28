933bcd8a2fca8e9eb952478a766657916103db14
Seonghuichoe <heeday0410@gmail.com>
2020-03-28 22:16:47 +0900

    feat: make jewel swap

--- 10_Bejeweled/main.cpp
+++ 10_Bejeweled/main.cpp
@@ -15,6 +15,15 @@ struct piece
   }
 } grid[10][10];
 
+void swap(piece p1, piece p2)
+{
+  std::swap(p1.col, p2.col);
+  std::swap(p1.row, p2.row);
+
+  grid[p1.row][p1.col] = p1;
+  grid[p2.row][p2.col] = p2;
+}
+
 int main()
 {
   srand(time(0));
@@ -37,6 +46,9 @@ int main()
       grid[i][j].x = j * ts;
       grid[i][j].y = i * ts;
     }
+  int x0, y0, x, y;
+  int click = 0;
+  Vector2i pos;
 
   while (app.isOpen())
   {
@@ -45,7 +57,47 @@ int main()
     {
       if (e.type == Event::Closed)
         app.close();
+
+      if (e.type == Event::MouseButtonPressed)
+        if (e.mouseButton.button == Mouse::Left)
+        {
+          click++;
+          pos = Mouse::getPosition(app) - offset;
+        }
     }
+
+    // mouse click
+    if (click == 1)
+    {
+      x0 = pos.x / ts + 1;
+      y0 = pos.y / ts + 1;
+    }
+    if (click == 2)
+    {
+      x = pos.x / ts + 1;
+      y = pos.y / ts + 1;
+      if (abs(x - x0) + abs(y - y0) == 1)
+      {
+        swap(grid[y0][x0], grid[y][x]);
+        click = 0;
+      }
+      else
+        click = 1;
+    }
+
+    //Moving animation
+    for (int i = 1; i <= 8; i++)
+      for (int j = 1; j <= 8; j++)
+      {
+        piece &p = grid[i][j];
+        int dx, dy;
+        dx = p.x - p.col * ts;
+        dy = p.y - p.row * ts;
+        if (dx)
+          p.x -= dx / abs(dx);
+        if (dy)
+          p.y -= dy / abs(dy);
+      }
 
     //////draw///////
     app.draw(background);
