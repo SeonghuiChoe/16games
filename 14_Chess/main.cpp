d3a70952770ee7f3f099da4f58ace6b66440f7a3
SeonghuiChoe <heeday0410@gmail.com>
2020-04-22 17:31:13 +0900

    feat: drag and drop

--- 14_Chess/main.cpp
+++ 14_Chess/main.cpp
@@ -11,14 +11,38 @@ int main()
 
   Sprite s(t1);
 
+  bool isMove = false;
+  float dx = 0, dy = 0;
+
   while (window.isOpen())
   {
+    Vector2i pos = Mouse::getPosition(window);
+
     Event e;
     while (window.pollEvent(e))
     {
       if (e.type == Event::Closed)
         window.close();
+
+      /////drag and drop///////
+      if (e.type == Event::MouseButtonPressed)
+        if (e.mouseButton.button == Mouse::Left)
+          if (s.getGlobalBounds().contains(pos.x, pos.y))
+          {
+            isMove = true;
+            dx = pos.x - s.getPosition().x;
+            dy = pos.y - s.getPosition().y;
+          }
+
+      if (e.type == Event::MouseButtonReleased)
+        if (e.mouseButton.button == Mouse::Left)
+        {
+          isMove = false;
+        }
     }
+    if (isMove)
+      s.setPosition(pos.x - dx, pos.y - dy);
+
     window.clear();
     window.draw(s);
     window.display();
