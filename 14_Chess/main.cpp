8367a97c7826b73dc3e932875421d3e0b7391e98
SeonghuiChoe <heeday0410@gmail.com>
2020-04-23 17:13:16 +0900

    feat: prepare and draw all chess on board

--- 14_Chess/main.cpp
+++ 14_Chess/main.cpp
@@ -2,21 +2,59 @@
 #include <time.h>
 using namespace sf;
 
+int size = 56;
+Vector2f offset(28, 28);
+
+Sprite f[32]; //figures
+
+int board[8][8] =
+    {-1, -2, -3, -4, -5, -3, -2, -1,
+     -6, -6, -6, -6, -6, -6, -6, -6,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0,
+     6, 6, 6, 6, 6, 6, 6, 6,
+     1, 2, 3, 4, 5, 3, 2, 1};
+
+void loadPosition()
+{
+  int k = 0;
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
+    {
+      int n = board[i][j];
+      if (!n)
+        continue;
+      int x = abs(n) - 1;
+      int y = n > 0 ? 1 : 0;
+      f[k].setTextureRect(IntRect(size * x, size * y, size, size));
+      f[k].setPosition(size * j, size * i);
+      k++;
+    }
+}
 int main()
 {
   RenderWindow window(VideoMode(504, 504), "The Chess! (press SPACE)");
 
-  Texture t1;
+  Texture t1, t2;
   t1.loadFromFile("images/figures.png");
+  t2.loadFromFile("images/board.png");
+
+  for (int i = 0; i < 32; i++)
+    f[i].setTexture(t1);
 
   Sprite s(t1);
+  Sprite sBoard(t2);
+
+  loadPosition();
 
   bool isMove = false;
   float dx = 0, dy = 0;
 
   while (window.isOpen())
   {
-    Vector2i pos = Mouse::getPosition(window);
+    Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
 
     Event e;
     while (window.pollEvent(e))
@@ -44,7 +82,14 @@ int main()
       s.setPosition(pos.x - dx, pos.y - dy);
 
     window.clear();
+    window.draw(sBoard);
     window.draw(s);
+    for (int i = 0; i < 32; i++)
+      f[i].move(offset);
+    for (int i = 0; i < 32; i++)
+      window.draw(f[i]);
+    for (int i = 0; i < 32; i++)
+      f[i].move(-offset);
     window.display();
   }
 
