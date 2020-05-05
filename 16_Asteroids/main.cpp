be7225276d986660bd94a820757eb7dafdfa18ed
Seonghuichoe <heeday0410@gmail.com>
2020-05-05 18:11:47 +0900

    feat: [asteroids] make spaceship move

--- 16_Asteroids/main.cpp
+++ 16_Asteroids/main.cpp
@@ -7,6 +7,8 @@ using namespace sf;
 const int W = 1200;
 const int H = 800;
 
+float DEGTORAD = 0.017453f;
+
 int main()
 {
   srand(time(0));
@@ -20,23 +22,70 @@ int main()
 
   Sprite sPlayer(t1), sBackground(t2);
   sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
-  sPlayer.setOrigin(-1160 / 2, -760 / 2);
+
+  float x = 300, y = 300;
+  float dx = 0, dy = 0, angle = 0;
+  bool thrust;
 
   /////main loop/////
   while (app.isOpen())
   {
-    Event event;
-    while (app.pollEvent(event))
+    Event e;
+    while (app.pollEvent(e))
     {
-      if (event.type == Event::Closed)
+      if (e.type == Event::Closed)
         app.close();
-
-      //////draw//////
-      app.clear();
-      app.draw(sBackground);
-      app.draw(sPlayer);
-      app.display();
     }
+
+    if (e.key.code == Keyboard::Right)
+      angle += 3;
+    if (e.key.code == Keyboard::Left)
+      angle -= 3;
+    if (e.key.code == Keyboard::Up)
+      thrust = true;
+    else
+      thrust = false;
+
+    //////spaceship movement//////
+    if (thrust)
+    {
+      dx += cos(angle * DEGTORAD) * 0.2;
+      dy += sin(angle * DEGTORAD) * 0.2;
+    }
+    else
+    {
+      dx *= 0.99;
+      dy *= 0.99;
+    }
+
+    int maxSpeed = 15;
+    float speed = sqrt(dx * dx + dy * dy);
+    if (speed > maxSpeed)
+    {
+      dx *= maxSpeed / speed;
+      dy *= maxSpeed / speed;
+    }
+
+    x += dx;
+    y += dy;
+
+    if (x > W)
+      x = 0;
+    if (x < 0)
+      x = W;
+    if (y > H)
+      y = 0;
+    if (y < 0)
+      y = H;
+
+    sPlayer.setPosition(x, y);
+    sPlayer.setRotation(angle + 90);
+
+    //////draw//////
+    app.clear();
+    app.draw(sBackground);
+    app.draw(sPlayer);
+    app.display();
   }
 
   return 0;
